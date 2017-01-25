485098d2719e2a24c754c093b966ac8b5babf912
Norman Joyner <norman.joyner@gmail.com>
2017-01-24 23:00:58 -0500

    Add DEFAULT_CLOUD_REGISTRY_BASE_URL (#6)

--- environment.js
+++ environment.js
@@ -3,5 +3,6 @@
 module.exports = {
     DEFAULT_AUTH_API_BASE_URL: process.env.CONTAINERSHIP_CLOUD_ENV === 'development' ? 'https://stage-auth.containership.io' : 'https://auth.containership.io',
     DEFAULT_BUILD_API_BASE_URL: process.env.CONTAINERSHIP_CLOUD_ENV === 'development' ? 'https://stage-build.containership.io' : 'https://build.containership.io',
-    DEFAULT_CLOUD_API_BASE_URL: process.env.CONTAINERSHIP_CLOUD_ENV === 'development' ? 'https://stage-api.containership.io' : 'https://api.containership.io'
+    DEFAULT_CLOUD_API_BASE_URL: process.env.CONTAINERSHIP_CLOUD_ENV === 'development' ? 'https://stage-api.containership.io' : 'https://api.containership.io',
+    DEFAULT_CLOUD_REGISTRY_BASE_URL: process.env.CONTAINERSHIP_CLOUD_ENV === 'development' ? 'https://stage-registry.containership.io' : 'https://registry.containership.io'
 }
